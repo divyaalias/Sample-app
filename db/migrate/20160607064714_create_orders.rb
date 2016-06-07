f29dfadbb21b2b31e253cd19b01bda0f6154c06e
Divya <divya@carmatec.com>
2016-06-07 12:26:04 +0530

    master

--- db/migrate/20160607064714_create_orders.rb
+++ db/migrate/20160607064714_create_orders.rb
@@ -1,8 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-    	t.datetime :order_date
-    	t.integer :customer_id 
+    	
       t.timestamps null: false
     end
   end
