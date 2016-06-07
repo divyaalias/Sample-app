1db2f5145341633e408c6841884c8087fe05bb4c
Divya <divya@carmatec.com>
2016-06-07 12:27:59 +0530

    belongs_to

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ActiveRecord::Base
+	belongs_to :customer
 end
+
+
+Order.last.customer   #this will give the customer details
+
+
--- db/migrate/20160607064714_create_orders.rb
+++ db/migrate/20160607064714_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-    	
+    	t.datetime :order_date
+    	t.integer :customer_id 
       t.timestamps null: false
     end
   end
