21bfd507b9865965681161fc7e53a168bb187139
Divya <divya@carmatec.com>
2016-06-07 12:47:30 +0530

    has_many

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ActiveRecord::Base
+	belongs_to :customer #this is not mandatory but some case we need query as Order.last.customer
 end
--- db/migrate/20160607064714_create_orders.rb
+++ db/migrate/20160607064714_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-    	
+    	t.integer :customer_id
       t.timestamps null: false
     end
   end
