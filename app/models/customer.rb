71315074bde9dad2f7a449ac1ecb473dea525452
Divya <divya@carmatec.com>
2016-06-07 12:39:56 +0530

    has_one

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,3 @@
 class Customer < ActiveRecord::Base
+	has_one :order #customer has only one order
 end
--- db/migrate/20160607064714_create_orders.rb
+++ db/migrate/20160607064714_create_orders.rb
@@ -1,7 +1,9 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-    	
+    	#t.integer :customer_id
+    	t.belongs_to :customer, index: true  #add customer_id to table
+    	t.string :order
       t.timestamps null: false
     end
   end
